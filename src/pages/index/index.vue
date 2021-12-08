30e880bb1f606028e9d4027882ee9fca9f3045ee
798648415 <798648415@qq.com>
2021-12-08 21:04:40 +0800

    格式化部分文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,54 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+      <button hover-class="button-hover" @click="startUser()">登录</button>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello World'
-			}
-		},
-		onLoad() {
-			
-		},
-		methods: {
+export default {
+  data() {
+    return {
+      title: "Hello World",
+    };
+  },
+  onLoad() {},
 
-		}
-	}
+  methods: {
+    /**
+     * 跳转“我的”页面
+     */
+    startUser() {
+      uni.redirectTo({ url: "/pages/user/user" });
+    },
+  },
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+  display: flex;
+  justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
 </style>
